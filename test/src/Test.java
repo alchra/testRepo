a8a11d59f0f20d7f79c6b3b3833cd7c4f42b97c9
alchra <alchra314@gmail.com>
2013-01-03 22:04:36 +0100

    commit

--- test/src/Test.java
+++ test/src/Test.java
@@ -3,17 +3,5 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Bye World!");
-		System.out.println("Bye World!");
-		System.out.println("Bye World!");
-		System.out.println("Ahoi World!");
-		System.out.println("Ahoi World!");
-		System.out.println("Ahoi World!");
-		System.out.println("now it works ?!?");
-		System.out.println("now it works ?!?");
-		System.out.println("now it works ?!?");
-		int miep;
 	}
 }
