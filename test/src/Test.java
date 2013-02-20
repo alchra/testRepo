112d2eed2705764b3b0750f38514d73b130d60a0
alchra <alchra314@gmail.com>
2013-02-20 16:19:41 +0100

    qwer

--- test/src/Test.java
+++ test/src/Test.java
@@ -3,16 +3,6 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("Bye World!");
-		System.out.println("Bye World!");
-		System.out.println("Bye World!");
-		System.out.println("Ahoi World!");
-		System.out.println("Ahoi World!");
-		System.out.println("Ahoi World!");
-		System.out.println("now it works ?!?");
-		System.out.println("now it works ?!?");
-		System.out.println("now it works ?!?");
+
 	}
 }
